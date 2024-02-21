efdb30f23255bf6b41d8224bd7fe229bd634a619
Ayuzhanna <ayuzhanasaaya@gmail.com>
2024-02-21 13:17:52 +0700

    создан проект

---
+++ app/src/main/java/com/example/pz7klimovapr23107/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pz7klimovapr23107;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
