82a619a09ba21dcfdb06f4bbab16fd05cbad7b44
Ayuzhanna <ayuzhanasaaya@gmail.com>
2024-02-21 13:59:14 +0700

    добавлен коментарий в 2 активность

---
+++ app/src/main/java/com/example/pz7klimovapr23107/Activity5.java
@@ -0,0 +1,14 @@
+package com.example.pz7klimovapr23107;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+    }
+}
